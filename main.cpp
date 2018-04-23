d5eebde1a5b209a868e781c366b82f8b2cc51ee3
Xavier Nye <xavieraleister694@gmail.com>
2018-04-22 19:31:55 -0500

    Added some code to make the triangle filled with a gradient and some code to prepare for dynamic color changing

--- main.cpp
+++ main.cpp
@@ -8,26 +8,37 @@ unsigned int WINDOW_WIDTH = 800;
 unsigned int WINDOW_HEIGHT = 600;
 unsigned int VBO;
 
-//create an array for verticies for the triangle
+//create an array of verticies and colors for the triangle
 float triangle_1_vert[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	//verticies         //colors
+	-0.5f, 0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
+	 0.5f, 0.0f, 0.0f,   0.9961f, 0.0039f, 0.9961f,
+	 0.0f, 0.5f, 0.0f,   0.0039, 0.9961f, 1.0f
 };
+
+//create an array for indicies
+//unsigned int indicies[] = {
+//	0, 1, 3, //first triangle
+//	2, 3, 1 //second triangle
+//};
 
 //create the vertex shader
 const char *vertexShaderSource = "#version 450 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main() {\n"	
-		"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 triangleColor;\n"
+"void main() {\n"
+	"gl_Position = vec4(aPos, 1.0);\n"
+	"triangleColor = aColor;\n"
+"}";
 
 //create the fragment shader
 const char *fragentShaderSource = "#version 450 core\n"
-	"out vec4 FragColor;\n"
-	"void main() {\n"
-		"FragColor = vec4(0.0f, 0.0f, 1.0f, 0.0f);\n"
-	"}\n";
+"out vec4 FragColor;\n"
+"in vec3 triangleColor;\n"
+"void main() {\n"
+	"FragColor = vec4(triangleColor, 1.0);\n"
+"}";
 
 //function to resize the viewport if the window is resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -60,7 +71,7 @@ int main() {
 	glfwMakeContextCurrent(window);
 	//callback to resize the viewport if the window is resized
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	
+
 	//if glad fails to initialize throw an error
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 		std::cout << "Failed to initialize GLAD" << std::endl;
@@ -69,7 +80,7 @@ int main() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_1_vert), triangle_1_vert, GL_STATIC_DRAW);
-	
+
 	//build and compile the vertex shader
 	//create a shader object, referenced by an ID
 	unsigned int vertexShader;
@@ -84,7 +95,7 @@ int main() {
 	//if compilation failed, print errors
 	if (!success) {
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-	std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
 	//build and compile the fragment shader
@@ -112,10 +123,7 @@ int main() {
 	//delete the shader programs to free up some space
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
+	
 	//create a vertex array object
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -125,19 +133,44 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_1_vert), triangle_1_vert, GL_STATIC_DRAW);
 	//set vertex attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	//set color attribute pointers
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	//create an element buffer object
+	//unsigned int EBO;
+	//glGenBuffers(1, &EBO);
+	//copy elements into a buffer
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies),	 indicies, GL_STATIC_DRAW);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 
 	//main render loop
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		//rendering commands
 		//set the window color
-		glClearColor(0.00, 1.00, 0.00, 1.00);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		//draw the triangle
+		//run the shader program
 		glUseProgram(shaderProgram);
+		//update the triangle color
+		float newColor[] = {
+			0.9451f, 0.9569f, 0.0078f
+		};
+
+		glBufferSubData(GL_ARRAY_BUFFER, 3 * sizeof(float), 3 * sizeof(float), newColor);
+
+		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "triangleColor");
+		//glUniform4f(vertexColorLocation, 0.0f, greenColor, 0.0f, 1.0f);
+
+		//draw the triangle
 		glBindVertexArray(VAO);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//swap the framebuffers to ensure smooth drawing
